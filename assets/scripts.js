6fa14f93a390d9471d4bedf9456b05c8ac9af9f8
gusr27 <gus.richardson@icloud.com>
2019-12-11 15:11:40 -0500

    Finished all styling, comments, and additions

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,5 +1,8 @@
+
+
 var json = JSON.parse(json);
 
+//this loads the json data into the project page
 function showHref(){
  var value = window.location.href.split('?')[1];
   
@@ -13,22 +16,22 @@ function showHref(){
 }    
 function changeTheme() {
     if(document.getElementById("themeChanger").style.color == "black"){
-document.body.style.backgroundColor = "gray";
+document.body.style.backgroundColor = "white";
    document.body.style.color = "pink";
    document.getElementById("themeChanger").style.color = "white";
     }else{
-        document.body.style.backgroundColor = "white";
-    document.body.style.color = "purple";
+        document.body.style.backgroundColor = "#282828";
+    document.body.style.color = "inherit";
         document.getElementById("themeChanger").style.color = "black";
     }
     
 }
-
+//uses alert to make sure the user reads something and selects okay
 function alertUser(){
     var nameVal = document.getElementById('contactName').value;
     alert('Thanks ' + nameVal +"! Your message was sent!");
 }
-
+//simple form validation
 function validateForm() {
   var x = document.forms["myForm"]["fname"].value;
   if (x == "") {
